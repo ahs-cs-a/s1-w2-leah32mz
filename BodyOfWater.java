867b8331fc6fe23e0233b361e4e51814e925423c
Leah <leahmz26@ausdk12.org>
2025-08-22 13:46:00 -0700

    Homework1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,21 @@
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
+
+    public BodyOfWater(String n, int l, int a, boolean i){
+        name = n;
+        largestDiameter = l;
+        avgDepth = a;
+        isSaltWater = i;
+
+    }
+
+    public BodyOfWater(String n, int a){
+        name = n;
+        avgDepth = a;
+    }
+
+}
 
